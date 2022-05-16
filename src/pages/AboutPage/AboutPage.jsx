e4eb55d9f665b1ef84ddbe29cc74be96ea90d26e
Armando Ubario <mandoubario@gmail.com>
2022-05-16 08:42:17 -0700

    Final Commit

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -4,7 +4,7 @@ export default function About ({
 }) {
     return (
         <div>
-        <h1>Abott Sneaker Stop </h1>
+        <h1>About Sneaker Stop </h1>
         <h3> Sneaker Stop is a web application that allows you keep track of your shoe value and keeps you updated with daily sneaker news.</h3>
         </div>
 
